280c8645a9438241d0ff4a3df3b7035e1a093204
Service Web <sw@gssmail.com>
2024-03-19 11:00:39 -0500

    Commit changes in C:\Websites\Git\emarlow\6632\GCG_6632_SN_RECORDS.sql

---
+++ GCG_6632_SN_RECORDS.sql
@@ -0,0 +1,14 @@
+CREATE TABLE "GCG_6632_SN_RECORDS" (
+ "Customer" CHAR(6),
+ "Part" CHAR(20),
+ "Prefix" CHAR(6),
+ "Mask" CHAR(20),
+ "Suffix" CHAR(6),
+ "Jump" INTEGER,
+ "Copies" INTEGER,
+ "Serial_Num" INTEGER,
+ "Serial_Full" CHAR(32),
+ "Job" CHAR(8),
+ "JS" CHAR(3),
+ "Date_SN" DATE,
+ "User_SN" CHAR(9) );
